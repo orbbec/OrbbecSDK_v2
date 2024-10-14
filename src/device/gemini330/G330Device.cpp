03ddaa29c96c7ac00f1493e4ebeb1fe84ecc027a
lixiaobin <lixiaobin@orbbec.com>
2024-10-14 16:20:24 +0800

    G335Le support firmware update

--- src/device/gemini330/G330Device.cpp
+++ src/device/gemini330/G330Device.cpp
@@ -1202,6 +1202,12 @@ void G330NetDevice::init() {
         return factory;
     });
 
+    registerComponent(OB_DEV_COMPONENT_FIRMWARE_UPDATER, [this]() {
+        std::shared_ptr<FirmwareUpdater> firmwareUpdater;
+        TRY_EXECUTE({ firmwareUpdater = std::make_shared<FirmwareUpdater>(this); })
+        return firmwareUpdater;
+    });
+
      registerComponent(OB_DEV_COMPONENT_COLOR_FRAME_METADATA_CONTAINER, [this]() {
          std::shared_ptr<FrameMetadataParserContainer> container;
          auto                                          envConfig                = EnvConfig::getInstance();
