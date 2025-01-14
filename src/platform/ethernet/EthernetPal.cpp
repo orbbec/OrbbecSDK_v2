9f4bd64636e3f746d4aefe82fb07c9f73b2c51fb
thinkbook <thinkbook@example.com>
2025-01-14 17:17:07 +0800

    Set DEVICE_WATCHER_POLLING_INTERVAL_MSEC to 5000

--- src/platform/ethernet/EthernetPal.cpp
+++ src/platform/ethernet/EthernetPal.cpp
@@ -10,7 +10,7 @@
 namespace libobsensor {
 
 const uint16_t DEFAULT_CMD_PORT                     = 8090;
-const uint16_t DEVICE_WATCHER_POLLING_INTERVAL_MSEC = 2000;
+const uint16_t DEVICE_WATCHER_POLLING_INTERVAL_MSEC = 5000;
 
 NetDeviceWatcher::~NetDeviceWatcher() noexcept {
     if(!stopWatch_) {
