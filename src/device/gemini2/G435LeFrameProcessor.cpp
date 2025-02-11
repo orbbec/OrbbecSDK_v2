6d08ad546b8f63459ef2d1f6a203f29860007d83
chaoxiaoshu <chaoxiaoshu@orbbec.com>
2025-02-11 17:20:12 +0800

    Temporarily delete RGB metadata

--- src/device/gemini2/G435LeFrameProcessor.cpp
+++ src/device/gemini2/G435LeFrameProcessor.cpp
@@ -32,40 +32,39 @@ G435LeColorFrameProcessor::G435LeColorFrameProcessor(IDevice *owner, std::shared
 G435LeColorFrameProcessor::~G435LeColorFrameProcessor() noexcept {}
 
 std::shared_ptr<Frame> G435LeColorFrameProcessor::process(std::shared_ptr<const Frame> frame) {
-    auto outFrame = FrameFactory::createFrameFromOtherFrame(frame);
+    return FrameProcessor::process(frame);
+    // auto outFrame = FrameFactory::createFrameFromOtherFrame(frame);
 
-    int index = findSequence(frame->getData(), static_cast<uint32_t>(frame->getDataSize()));
-    if (index == -1) {
-        LOG_ERROR("findSequence not found");
-        return FrameProcessor::process(frame);
-    }
+    // int index = findSequence(frame->getData(), static_cast<uint32_t>(frame->getDataSize()));
+    // if (index == -1) {
+    //     return FrameProcessor::process(frame);
+    // }
     
-    const int fixedDistanse = 14; 
+    // const int fixedDistanse = 14; 
     
-    if (index + 14 >= frame->getDataSize()) {
-        LOG_ERROR("Index out of bounds");
-        return FrameProcessor::process(frame);
-    }    
+    // if (index + 14 >= frame->getDataSize()) {
+    //     return FrameProcessor::process(frame);
+    // }    
 
-    G435LeMetadataHearder *header = reinterpret_cast<G435LeMetadataHearder *>(const_cast<uint8_t *>(frame->getData() + index + fixedDistanse));
+    // G435LeMetadataHearder *header = reinterpret_cast<G435LeMetadataHearder *>(const_cast<uint8_t *>(frame->getData() + index + fixedDistanse));
 
-    if(!header->validateMagic()) {
-        return FrameProcessor::process(frame);
-    }
+    // if(!header->validateMagic()) {
+    //     return FrameProcessor::process(frame);
+    // }
 
-    const int metadatasize = header->data_len;
+    // const int metadatasize = header->data_len;
 
-    if (index + fixedDistanse + metadatasize >= frame->getDataSize()) {
-        LOG_ERROR("Index out of bounds");
-        return FrameProcessor::process(frame);
-    }
+    // if (index + fixedDistanse + metadatasize >= frame->getDataSize()) {
+    //     LOG_ERROR("Index out of bounds");
+    //     return FrameProcessor::process(frame);
+    // }
 
-    std::vector<uint8_t> data;
-    data.assign(frame->getData(), frame->getData() + index + fixedDistanse - 4);
-    data.insert(data.end(), frame->getData() + index + fixedDistanse + metadatasize, frame->getData() + frame->getDataSize());
+    // std::vector<uint8_t> data;
+    // data.assign(frame->getData(), frame->getData() + index + fixedDistanse - 4);
+    // data.insert(data.end(), frame->getData() + index + fixedDistanse + metadatasize, frame->getData() + frame->getDataSize());
 
-    outFrame->updateData(data.data(), data.size());
-    outFrame->updateMetadata(frame->getData() + index + fixedDistanse, metadatasize);
+    // outFrame->updateData(data.data(), data.size());
+    // outFrame->updateMetadata(frame->getData() + index + fixedDistanse, metadatasize);
 
     // std::ofstream outputFileOrg("frame_data_org.bin", std::ios::binary); 
     // if(outputFileOrg.is_open()) {
@@ -84,7 +83,7 @@ std::shared_ptr<Frame> G435LeColorFrameProcessor::process(std::shared_ptr<const 
     //     outputFileMetadata.write(reinterpret_cast<const char *>(outFrame->getMetadata()), outFrame->getMetadataSize());  
     //     outputFileMetadata.close();                                                                        
     // }
-    return FrameProcessor::process(outFrame);
+    //return FrameProcessor::process(outFrame);
 }
 int G435LeColorFrameProcessor::findSequence(const uint8_t *data, uint32_t size) {
     const uint8_t target[]     = { 0xFF, 0xDA, 0x00, 0x0C };
