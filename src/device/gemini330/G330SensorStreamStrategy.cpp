c36fe109dcc00402f7bfa530bae7c3182b75df2a
zhougong <zhougong@orbbec.com>
2025-06-20 18:14:26 +0800

    Disallowed Y12 and Y16 formats for IR streaming on G330 devices when not in Factory mode

--- src/device/gemini330/G330SensorStreamStrategy.cpp
+++ src/device/gemini330/G330SensorStreamStrategy.cpp
@@ -115,7 +115,7 @@ void G330SensorStreamStrategy::validateDepthAndIrStream(const std::vector<std::s
 }
 
 void G330SensorStreamStrategy::validatePreset(const std::vector<std::shared_ptr<const StreamProfile>> &profiles) {
-    OBDepthWorkMode_Internal currentDepthMode;
+    OBDepthWorkMode_Internal currentDepthMode{};
 
     {
         auto owner                = getOwner();
@@ -123,10 +123,11 @@ void G330SensorStreamStrategy::validatePreset(const std::vector<std::shared_ptr<
         currentDepthMode          = depthWorkModeManager->getCurrentDepthWorkMode();
     }
 
-    for(auto profile: profiles) {
-        auto        streamType  = profile->getType();
-        const char *FactoryMode = "Factory Calib";
-        if(strncmp(currentDepthMode.name, FactoryMode, strlen(FactoryMode)) == 0) {
+    const char *FactoryMode = "Factory Calib";
+    if(strncmp(currentDepthMode.name, FactoryMode, strlen(FactoryMode)) == 0) {
+        // Factory Calibration mode
+        for(auto profile: profiles) {
+            auto        streamType  = profile->getType();
             switch(streamType) {
             case OB_STREAM_IR_LEFT:
             case OB_STREAM_IR_RIGHT: {
@@ -135,13 +136,29 @@ void G330SensorStreamStrategy::validatePreset(const std::vector<std::shared_ptr<
                 auto height = vsp->getHeight();
                 auto format = vsp->getFormat();
                 if(!(((width == 1280 && height == 800) || (width == 640 && height == 400)) && (format == OB_FORMAT_Y12 || format == OB_FORMAT_Y16))) {
-                    throw unsupported_operation_exception("Preset is in Factory Calib mode, only support IR streams at 1280x800@Y12/Y16 and 640x400@Y12/Y16");
+                    throw unsupported_operation_exception("Preset is in Factory Calibration mode, only support IR streams at 1280x800@Y12/Y16 and 640x400@Y12/Y16");
                 }
             } break;
             case OB_STREAM_DEPTH:
-                throw unsupported_operation_exception("Preset is in Factory Calib mode, dose not support Depth stream");
+                throw unsupported_operation_exception("Preset is in Factory Calibration mode, dose not support Depth stream");
                 break;
-
+            default:
+                break;
+            }
+        }
+    }
+    else {
+        // Non-Factory Calibration mode
+        for(auto profile: profiles) {
+            auto streamType = profile->getType();
+            switch(streamType) {
+            case OB_STREAM_IR_LEFT:
+            case OB_STREAM_IR_RIGHT: {
+                auto format = profile->getFormat();
+                if(format == OB_FORMAT_Y12 || format == OB_FORMAT_Y16) {
+                    throw unsupported_operation_exception("The current preset is not in Factory Calibration mode; IR Y12/Y16 formats are not supported");
+                }
+            } break;
             default:
                 break;
             }
