90e295bed9bf1f53c5ef3b3380493778e1d82abb
zhonghong <zhonghong@orbbec.com>
2025-07-15 13:40:51 +0800

    Add Gemini 330 series and Dabai A series pid in metadata script

--- scripts/env_setup/obsensor_metadata_win10.ps1
+++ scripts/env_setup/obsensor_metadata_win10.ps1
@@ -34,8 +34,18 @@ $SearchTrees = "$DevConfigRegBase\$guid1", "$DevConfigRegBase\$guid2"
 
 # Multipin devices that need additional key MetadataBufferSizeInKB1 & MetadataBufferSizeInKB2 & etc...
 $MultiPinDevices = "USB\VID_2BC5&PID_06D0&MI_00", #  Gemini 2 R  // 3 pin for depth uvc
-                 "USB\VID_2BC5&PID_06D1&MI_00" #  Gemini 2 RL  // 3 pin for depth uvc
-
+                 "USB\VID_2BC5&PID_06D1&MI_00", #  Gemini 2 RL  // 3 pin for depth uvc
+                 "USB\VID_2BC5&PID_0800&MI_00",
+                 "USB\VID_2BC5&PID_0801&MI_00",
+                 "USB\VID_2BC5&PID_0803&MI_00",
+                 "USB\VID_2BC5&PID_0804&MI_00",
+                 "USB\VID_2BC5&PID_0805&MI_00",
+                 "USB\VID_2BC5&PID_0807&MI_00",
+                 "USB\VID_2BC5&PID_080B&MI_00",
+                 "USB\VID_2BC5&PID_0A12&MI_00",
+                 "USB\VID_2BC5&PID_0A13&MI_00",
+                 "USB\VID_2BC5&PID_0812&MI_00",
+                 "USB\VID_2BC5&PID_0813&MI_00"
 #Inhibit system warnings and erros, such as permissions or missing values
 $ErrorActionPreference = "silentlycontinue"
 
