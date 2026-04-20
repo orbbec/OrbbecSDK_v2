8b4b3b9b91c0e034c458bc208441c687773bd674
simbazhou <zhougong@orbbec.com>
2026-04-20 15:46:49 +0800

    Use steady_clock instead of system_clock for FPS calculation to avoid issues from NTP/system time adjustments

--- src/shared/logger/LoggerHelper.hpp
+++ src/shared/logger/LoggerHelper.hpp
@@ -25,28 +25,28 @@ struct ObLogRecord {
 #pragma clang diagnostic pop
 #endif
 // Count the call frequency and then output the log
-#define LOG_FREQ_CALC(level, duration, msg, ...)                                                                                           \
-    do {                                                                                                                                   \
-        static std::map<std::string, std::shared_ptr<ObLogRecord>> logIntvlRecordMap_;                                                     \
-        static std::mutex                                          mtx;                                                                    \
-        std::unique_lock<std::mutex>                               logLock(mtx);                                                           \
-        auto                                                       iter = logIntvlRecordMap_.find(LOG_FREQ_CALC_OBJECT_TAG);               \
-        uint64_t now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count(); \
-        if(iter == logIntvlRecordMap_.end()) {                                                                                             \
-            auto record      = std::make_shared<ObLogRecord>();                                                                            \
-            record->count    = 0;                                                                                                          \
-            record->lastTime = now;                                                                                                        \
-            logIntvlRecordMap_.insert(std::make_pair(LOG_FREQ_CALC_OBJECT_TAG, record));                                                   \
-            iter = logIntvlRecordMap_.find(LOG_FREQ_CALC_OBJECT_TAG);                                                                      \
-        }                                                                                                                                  \
-        iter->second->count++;                                                                                                             \
-        if(now - iter->second->lastTime > duration) {                                                                                      \
-            float       freq   = iter->second->count / ((now - iter->second->lastTime) / 1000.0f);                                         \
-            std::string outMsg = msg;                                                                                                      \
-            outMsg.replace(outMsg.find("{freq}"), 6, std::to_string(freq));                                                                \
-            LOG_##level(outMsg VA_ARGS(__VA_ARGS__));                                                                                      \
-            iter->second->count    = 0;                                                                                                    \
-            iter->second->lastTime = now;                                                                                                  \
-        }                                                                                                                                  \
+#define LOG_FREQ_CALC(level, duration, msg, ...)                                                                             \
+    do {                                                                                                                     \
+        static std::map<std::string, std::shared_ptr<ObLogRecord>> logIntvlRecordMap_;                                       \
+        static std::mutex                                          mtx;                                                      \
+        std::unique_lock<std::mutex>                               logLock(mtx);                                             \
+        auto                                                       iter = logIntvlRecordMap_.find(LOG_FREQ_CALC_OBJECT_TAG); \
+        uint64_t                                                   now  = utils::getSteadyTimeMs();                          \
+        if(iter == logIntvlRecordMap_.end()) {                                                                               \
+            auto record      = std::make_shared<ObLogRecord>();                                                              \
+            record->count    = 0;                                                                                            \
+            record->lastTime = now;                                                                                          \
+            logIntvlRecordMap_.insert(std::make_pair(LOG_FREQ_CALC_OBJECT_TAG, record));                                     \
+            iter = logIntvlRecordMap_.find(LOG_FREQ_CALC_OBJECT_TAG);                                                        \
+        }                                                                                                                    \
+        iter->second->count++;                                                                                               \
+        if(now - iter->second->lastTime > duration) {                                                                        \
+            float       freq   = iter->second->count / ((now - iter->second->lastTime) / 1000.0f);                           \
+            std::string outMsg = msg;                                                                                        \
+            outMsg.replace(outMsg.find("{freq}"), 6, std::to_string(freq));                                                  \
+            LOG_##level(outMsg VA_ARGS(__VA_ARGS__));                                                                        \
+            iter->second->count    = 0;                                                                                      \
+            iter->second->lastTime = now;                                                                                    \
+        }                                                                                                                    \
     } while(0)
 
